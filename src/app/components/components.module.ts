564c4de9cc063c061f6283e8151e2931599d8e7a
David <davidaquos@gmail.com>
2021-03-12 12:53:41 +0100

    Fabicon terminado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,17 +4,18 @@ import {MenuComponent} from './menu/menu.component';
 import {IonicModule} from '@ionic/angular';
 import {RouterModule} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {FabIconComponent} from './fab-icon/fab-icon.component';
 
 
 
 @NgModule({
   declarations: [HeaderComponent,
-    MenuComponent],
+    MenuComponent, FabIconComponent],
   imports: [
     CommonModule,
     IonicModule,
     RouterModule,
   ],
-  exports: [MenuComponent, HeaderComponent]
+  exports: [MenuComponent, HeaderComponent, FabIconComponent]
 })
 export class ComponentsModule { }
