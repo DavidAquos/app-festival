5a165fb8270e96fef8050e2bd59708ff5c688e76
Eduard Guillot <eduard.guillot.alvarez@cieep.com>
2021-03-17 09:54:56 +0100

    popover

--- src/app/pages/comprartickets/comprartickets.page.ts
+++ src/app/pages/comprartickets/comprartickets.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TicketPrecios} from '../../interface/interface';
 import {DataService} from '../../services/data.service';
-import {AlertController} from '@ionic/angular';
+import {AlertController, PopoverController} from '@ionic/angular';
+import {CartComponent} from '../../components/cart/cart.component';
 
 @Component({
   selector: 'app-comprartickets',
@@ -11,7 +12,7 @@ import {AlertController} from '@ionic/angular';
 export class ComprarticketsPage implements OnInit {
 
   listTickets: TicketPrecios[];
-  constructor(private dataService: DataService, public alertController: AlertController) { }
+  constructor(private dataService: DataService, public alertController: AlertController, public popoverController: PopoverController) { }
 
   ngOnInit() {
     this.dataService.getTicketsVenta().subscribe(tickets => {
@@ -22,14 +23,12 @@ export class ComprarticketsPage implements OnInit {
 
   async Alert(nombre: string) {
     const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
       header: nombre,
       message: 'Añadir al <strong>Carrito</strong>?',
       buttons: [
         {
           text: 'Cancel',
           role: 'cancel',
-          cssClass: 'secondary',
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
           }
@@ -42,5 +41,13 @@ export class ComprarticketsPage implements OnInit {
       ]
     });
     await alert.present();
+  }
+
+  async popover() {
+    const popover = await this.popoverController.create({
+      component: CartComponent,
+      translucent: true
+    });
+    return await popover.present();
   }
 }
