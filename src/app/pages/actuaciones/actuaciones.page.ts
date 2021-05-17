a131bae8299938d630ec6f586324c8713d4a2103
David <davidaquos@gmail.com>
2021-05-17 11:32:57 +0200

    loader

--- src/app/pages/actuaciones/actuaciones.page.ts
+++ src/app/pages/actuaciones/actuaciones.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Actuacion} from '../../interface/interface';
 import {DataService} from '../../services/data.service';
+import {LoadingController, ModalController} from "@ionic/angular";
 
 @Component({
   selector: 'app-actuaciones',
@@ -14,7 +15,7 @@ export class ActuacionesPage implements OnInit {
   listActuaciones: Actuacion[] = [];
   actuaciones: Actuacion[] = [];
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, public loadingController: LoadingController) { }
 
   ngOnInit() {
     this.dataService.getActuaciones().subscribe(res => {
@@ -22,6 +23,7 @@ export class ActuacionesPage implements OnInit {
       this.listActuaciones.push(...this.actuaciones.splice(0, 4));
       this.initialLength = this.listActuaciones.length;
     });
+    this.presentLoading();
   }
 
   doInfinite(event: any) {
@@ -34,5 +36,16 @@ export class ActuacionesPage implements OnInit {
       this.listActuaciones.push(...this.actuaciones.splice(0, 4));
       event.target.complete();
     }, 500);
+  }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Cargando...',
+      duration: 2000
+    });
+
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
   }
 }
